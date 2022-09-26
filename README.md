ab3c3480e41ccba2566b942739c06c92d7f70d4c
glasiore <herr.guclu@gmail.com>
2022-09-27 00:21:16 +0200

    feat: First Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ceycey
+hi
---
+++ script.js
@@ -0,0 +1,57 @@
+// // var x;
+// // x="Emre"
+// // console.log(x);
+
+// // let fullname="Emre Güçlü";
+// // console.log(fullname);
+
+// // const email="hiimglass@gmail.com";
+// // console.log(email);
+
+
+// //Değişken Türleri
+
+// //Primitive Types
+
+// var x;
+
+
+// //null
+// var y=null;
+
+// //string
+// var name="Emre";
+
+// //number
+// var age = 24
+
+// //boolean
+// var online = true
+
+
+
+// //Reference Types - Objects de deniyor
+
+// //Array
+
+// var cars = ["Volvo","BMW","Mercedes"]
+
+// //Objects
+
+// var person= {
+//     name:"Emre",
+//     age:24
+// }
+
+// //function
+
+// var isAlive = function(){
+//     return 0;
+// }
+
+// console.log(typeof isAlive);
+
+let x = 15;
+a = x + 7;
+
+console.log(a)
